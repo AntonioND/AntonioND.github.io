c687c8614cf6b6f5379dc66c865ebec43ebc9931
Antonio Niño Díaz <antonio_nd@outlook.com>
2018-02-05 00:51:41 +0000

    Add initial version of website
    Signed-off-by: Antonio Niño Díaz <antonio_nd@outlook.com>

---
+++ theme/js/bodypadding.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    if( (window.location.pathname.indexOf("/index") == 0)
+     || (window.location.pathname == '/') ) {
+       $('body').css('padding-top', $('.navbar').height()+'px');
+    }
+});
